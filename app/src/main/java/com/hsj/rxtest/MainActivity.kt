6e4f037ee7d4102d7d4b2b53bd4e568c91029a13
JoHongsik <queue1991@naver.com>
2022-02-27 22:32:53 +0900

    add just

--- app/src/main/java/com/hsj/rxtest/MainActivity.kt
+++ app/src/main/java/com/hsj/rxtest/MainActivity.kt
@@ -5,18 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableEmitter
 
+/**
+ * onNext() : 하나의 소스 Observable에서 Observer까지 한 번에 하나씩 순차적으로 데이터를 발행한다.
+ * onComplete() : 데이터 발행이 끝났음을 알리는 완료 이벤트를 Observer에 전달하여 onNext()를 더 호출하지 않음을 나타낸다.
+ * onError() : 오류가 발생했음을 Observer에 전달한다.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        observableWithCreate()
+        observableWithJust()
     }
 
+
     /**
-     * onNext() : 하나의 소스 Observable에서 Observer까지 한 번에 하나씩 순차적으로 데이터를 발행한다.
-     * onComplete() : 데이터 발행이 끝났음을 알리는 완료 이벤트를 Observer에 전달하여 onNext()를 더 호출하지 않음을 나타낸다.
-     * onError() : 오류가 발생했음을 Observer에 전달한다.
+     * Observable.create()를 사용하면 Emitter를 이용하여 직접 아이템을 발행하고,
+     * 아이템 발행의 완료나 오류(Complete/Error)의 알림을 직접 설정할 수 있다.
      */
     private fun observableWithCreate(){
 //        val observable: Observable<String> = Observable.create { emitter ->
@@ -26,17 +31,27 @@ class MainActivity : AppCompatActivity() {
 //        }
 //        observable.subscribe(System.out::println)
 
-        val source = Observable.create { emitter: ObservableEmitter<String?> ->
+        val observable = Observable.create { emitter: ObservableEmitter<String?> ->
             emitter.onNext("Hello")
             emitter.onError(Throwable())
             emitter.onNext("Yena")
         }
-        source.subscribe(
+        observable.subscribe(
             { x: String? -> println(x) }
         ) { throwable: Throwable? ->
             println(
                 "Good bye"
             )
         }
+    }
+
+    /**
+     * just()는 해당 아이템을 그대로 발행하는 Observable을 생성해준다.
+     * just() 연산자의 인자로 넣은 아이템을 차례로 발행하며,
+     * 한 개의 아이템을 넣을 수도 있고, 타입이 같은 여러 아이템을 넣을 수도 있다.
+     */
+    private fun observableWithJust(){
+        val observable : Observable<String> = Observable.just("Hello","just")
+        observable.subscribe(System.out::println)
     }
 }
