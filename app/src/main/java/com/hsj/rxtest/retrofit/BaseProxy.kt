0c76c40a2c7c168c347bf77b3ccc8172fd96e5d0
JoHongsik <queue1991@naver.com>
2022-02-28 22:42:46 +0900

    add calling retrofit using single

---
+++ app/src/main/java/com/hsj/rxtest/retrofit/BaseProxy.kt
@@ -0,0 +1,52 @@
+package com.hsj.rxtest.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+open class BaseProxy(connectSeconds: Long = DEFAULT_CONNECT_SECONDS,
+                     writeSeconds: Long = DEFAULT_WRITE_SECONDS,
+                     readSeconds: Long = DEFAULT_READ_SECONDS
+) : Proxy(connectSeconds, writeSeconds, readSeconds) {
+    override fun onCreateOkHttpClient(builder: OkHttpClient.Builder): OkHttpClient {
+        builder.addInterceptor(appInterceptor)
+        builder.addNetworkInterceptor(netInterceptor)
+        return super.onCreateOkHttpClient(builder)
+    }
+
+    override fun onCreateRetrofit(builder: Retrofit.Builder): Retrofit {
+        builder.addConverterFactory(GsonConverterFactory.create())
+        return builder.build()
+    }
+
+    protected fun howserUrl(path: String): String {
+        return "https/$path"
+    }
+
+
+    companion object {
+        private const val TYPE_DELIVERY = "GOODS"
+        private const val TYPE_MEASURE = "MEASURE"
+        private const val TYPE_SPOT = "SPOT"
+
+        /**
+         * App -> Server Request Header 정의
+         */
+        val appInterceptor: AppInterceptor = object : AppInterceptor() {
+            override val authorization: String?
+                get() = TODO("Not yet implemented")
+            override val userAgent: String?
+                get() = TODO("Not yet implemented")
+        }
+
+        /**
+         * Server -> App Response Header 정의
+         */
+        val netInterceptor: NetInterceptor = object : NetInterceptor() {
+            override fun onAuthorizationUpdated(newToken: String) {
+//                if (Pref.accessToken != newToken)
+//                    Pref.accessToken = newToken
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/hsj/rxtest/retrofit/Proxy.kt
@@ -0,0 +1,43 @@
+package com.hsj.rxtest.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * proxy + addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+ */
+abstract class Proxy protected constructor(
+    private val connectSeconds: Long = DEFAULT_CONNECT_SECONDS,
+    private val writeSeconds: Long = DEFAULT_WRITE_SECONDS,
+    private val readSeconds: Long = DEFAULT_READ_SECONDS
+) {
+
+    open fun retrofit(baseUrl: String): Retrofit {
+        val clientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+                .connectTimeout(connectSeconds, TimeUnit.SECONDS)
+                .writeTimeout(writeSeconds, TimeUnit.SECONDS)
+                .readTimeout(readSeconds, TimeUnit.SECONDS)
+        val client: OkHttpClient = onCreateOkHttpClient(clientBuilder)
+
+        val retrofitBuilder = Retrofit.Builder()
+                .client(client)
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .baseUrl(baseUrl)
+        return onCreateRetrofit(retrofitBuilder)
+    }
+
+    open fun onCreateOkHttpClient(builder: OkHttpClient.Builder): OkHttpClient {
+        return builder.build()
+    }
+
+
+    abstract fun onCreateRetrofit(builder: Retrofit.Builder): Retrofit
+
+    companion object {
+        const val DEFAULT_CONNECT_SECONDS = 10L
+        const val DEFAULT_WRITE_SECONDS = 10L
+        const val DEFAULT_READ_SECONDS = 60L
+    }
+}
